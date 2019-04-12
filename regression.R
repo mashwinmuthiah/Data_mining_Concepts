3d73dbb596896aadb71fc35ba20c3121029423b1
mashwinmuthiah <ashwin.muthiah1996@gmail.com>
2019-04-12 12:50:24 -0400

    12apr

--- regression.R
+++ regression.R
@@ -46,3 +46,49 @@ summary(fit4)
 install.packages("effects")
 library(effects)
 plot(effect("hp:wt",fit4,xlevels = list(wt=c(2.2,3.2,4.2))),multiline = TRUE)
+
+#regression diagnostics
+confint(fit3)
+
+#A Typical approch
+par(mfrow = c(2,2))
+plot(myfit)
+
+plot(fit2)
+
+newfit2<-lm(weight~height+I(height^2),data = women[-c(13,15),])
+plot(newfit2)
+
+plot(fit3)
+
+#An enhanced approch
+## Normality
+library(car)
+par(mfrow=c(1,1))
+qqPlot(fit3, labels=row.names(states), id.method="identify",
+       simulate=TRUE, main="Q-Q Plot")
+
+residplot <- function(fit, nbreaks=10) {
+  z <- rstudent(fit)
+  hist(z, breaks=nbreaks, freq=FALSE)
+  curve(dnorm(x, mean=mean(z), sd=sd(z)),
+        add=TRUE, col="blue", lwd=2)
+  lines(density(z)$x, density(z)$y,
+        col="red", lwd=2, lty=2)
+}
+residplot(fit3)
+
+#independence
+durbinWatsonTest(fit3)
+
+#linearity
+crPlots(fit3)
+
+#homoscendasticity
+ncvTest(fit3)
+spreadLevelPlot(fit3) 
+
+#global test
+library(gvlma)
+gvmode<- gvlma(fit3)
+summary(gvmode)
